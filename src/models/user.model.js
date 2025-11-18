02bab8d7fc561cc61c1a9843e65392448f680d76
Ayush Sood <cgent.hpu@gmail.com>
2025-11-18 16:43:56 +0530

    update methods for generating access web tokens and refresh web tokens

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,5 +1,7 @@
 import mongoose from "mongoose";
 import bcrypt from "bcrypt";
+import crypto from "crypto";
+import jwt from "jsonwebtoken";
 
 const userSchema = new mongoose.Schema(
   {
@@ -66,6 +68,38 @@ userSchema.pre("save", async function () {
 
 userSchema.methods.checkPassword = async function (password) {
   return await bcrypt.compare(password, this.password);
+};
+
+userSchema.methods.generateAccessToken = async function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+      username: this.username,
+      email: this.email,
+    },
+    process.env.accessTokenSecret,
+    { expiresIn: process.env.accessTokenExpiry }
+  );
+};
+
+userSchema.methods.generateRefreshToken = async function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+    },
+    process.env.refreshTokenSecret,
+    { expiresIn: process.env.refreshTokenExpiry }
+  );
+};
+
+userSchema.methods.createTemporaryToken = async function () {
+  const unhashedToken = crypto.randomBytes(20).toString("hex");
+  const hashedToken = crypto
+    .createHash("sha256")
+    .update(unhashedToken)
+    .digest("hex");
+  const tokenExpiry = Date.now + 20 * 60 * 1000;
+  return { unhashedToken, hashedToken, tokenExpiry };
 };
 const User = mongoose.model("User", userSchema);
 export default User;
