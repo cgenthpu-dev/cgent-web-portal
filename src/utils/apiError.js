4edfefcd862650431ae8f1f5b3aff752c5afcd7b
Ayush Sood <cgent.hpu@gmail.com>
2025-11-25 16:33:16 +0530

    auth is done

--- src/utils/apiError.js
+++ src/utils/apiError.js
@@ -1,25 +1,22 @@
-class ApiResponseError extends Error {
+class ApiError extends Error {
   constructor(
     statusCode,
-    data = null,
     message = "Something went wrong",
     errors = [],
     stack
   ) {
     super(message);
     this.statusCode = statusCode;
-    this.data = null;
-    this.data = data;
     this.message = message;
     this.isSuccess = statusCode > 400;
-    this.error = errors;
+    this.errors = errors;
 
     if (!stack) {
-      this.stack = this.stack;
+      this.stack = stack;
     } else {
       this.stack = Error.captureStackTrace(this, this.constructor);
     }
   }
 }
 
-export { ApiResponseError };
+export { ApiError };
